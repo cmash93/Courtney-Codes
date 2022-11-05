e2a9375b9819e9add536a81a93a4b56a47d55eaa
cmash93 <cmash93@gmail.com>
2022-11-05 00:20:36 -0500

    worked on dashboard and home routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -5,6 +5,33 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.params.user_id
+            },
+            attributes: ['id', 'title', 'post_body', 'date_created'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_body', 'post_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            ]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            posts,
+            logged_in: req.session.logged_in
+        });
 
     } catch (err) {
         res.status(500).json(err)
@@ -13,16 +40,76 @@ router.get('/', withAuth, async (req, res) => {
 
 router.get('/edit/:id', withAuth, async (req, res) => {
     try {
+        const updatedPost = await Post.findByPk({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id', 'title', 'post_body', 'date_created'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_body', 'post_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            ]
+        });
 
+        if (!updatedPost) {
+            res.status(404).json({message: "No post found!"})
+        }
+
+        const post = updatedPost.get({ plain: true });
+
+        res.render('edit-post', {
+            post,
+            logged_in: req.session.logged_in
+        })
+    
     } catch (err) {
         res.status(500).json(err)
     }
 });
 
-router.get('/new', withAuth, async (req, res) => {
+router.get('/create', withAuth, async (req, res) => {
     try {
+        const createPost = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: ['id', 'title', 'post_body', 'date_created'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_body', 'post_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            ]
+        })
+
+        const newPost = createPost.map((post) => post.get({ plain: true }));
+
+        res.render('create-post', {
+            posts,
+            logged_in: req.session.logged_in
+        })
 
     } catch (err) {
         res.status(500).json(err)
     }
 });
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,29 +2,68 @@ const router = require('express').Router();
 const sequelize= require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
+
 router.get('/', async (req, res) => {
     try {
+        const postData = await Post.findAll({
+            attributes: ['id', 'title', 'post_body', 'date_created'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_body', 'post_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            ]
+        });
 
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500).json(err)
     }
 });
+
 
 router.get('/login', async (req, res) => {
     try {
+        if (req.session.logged_in) {
+            res.redirect('/');
+            return;
+        }
+
+        res.render('login')
 
     } catch (err) {
         res.status(500).json(err)
     }
 });
+
 
 router.get('/signup', async (req, res) => {
     try {
+        if(req.session.logged_in) {
+            res.redirect('/');
+            return;
+        }
+
+        res.render('signup')
 
     } catch (err) {
         res.status(500).json(err)
     }
 });
+
 
 router.get('/posts/:id', async (req, res) => {
     try {
@@ -34,9 +73,40 @@ router.get('/posts/:id', async (req, res) => {
     }
 });
 
+
 router.get('/comments', async (req, res) => {
     try {
+        const singlePost = await Post.findByPk({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id', 'title', 'post_body', 'date_created'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_body', 'post_id', 'user_id', 'date_created'],
+                    include: {
+                        model: User,
+                        attributes: ['username', 'github']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            ]
+        });
 
+        if (!singlePost) {
+            res.status(404).json({message: "No post found!"})
+        }
+
+        const post = singlePost.get({ plain: true });
+
+        res.render('single-post', {
+            post,
+            logged_in: req.session.logged_in
+        })
     } catch (err) {
         res.status(500).json(err)
     }
