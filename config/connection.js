d80e7e9a7b0399b4c8eb13deecec10637e9dd3f7
cmash93 <cmash93@gmail.com>
2022-11-08 16:17:23 -0600

    >:(

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres'
-    });
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
     } else {
         sequelize = new Sequelize(
         process.env.DB_NAME,
